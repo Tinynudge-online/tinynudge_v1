8d01e46ca3e5447e05e6958b0081318017f233c0
Tinynudge-online <tinynudge@gmail.com>
2026-05-21 17:21:02 -0700

    Use env vars for Supabase client in src/lib/supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,11 @@
 // lib/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://uaqbwsmuuvaukxurzdks.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhcWJ3c211dXZhdWt4dXJ6ZGtzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUwNTI4NzEsImV4cCI6MjA3MDYyODg3MX0.V__PztLOgmJO40UE7Nf3OcQRYojURZsKvpH-hp-XurU'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables')
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
